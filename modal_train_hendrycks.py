9dc2125fa46bdf3ba017a65db1bc70c5d8a7cd3e
Grebenchtchikova <greben@stanford.edu>
2026-05-30 18:35:31 +0100

    Update modal_train_hendrycks.py

--- modal_train_hendrycks.py
+++ modal_train_hendrycks.py
@@ -23,7 +23,8 @@ app = modal.App("cs224r-trivia-train-hendrycks")
 
 image = (
     modal.Image.from_dockerfile("docker/Dockerfile.ngc.vllm0.8.noverl")
-    .run_commands("pip install --no-deps verl==0.2.0.post2")
+    .add_local_dir("../cs224r-project-e3", "/root/verl", copy=True)
+    .run_commands("pip install -e /root/verl")
     .add_local_file("scripts/grpo_hendrycks_a100.sh", "/root/grpo_hendrycks_a100.sh")
 )
 
