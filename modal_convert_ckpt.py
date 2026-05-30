8fe70c6d616d23f9b5a907719176767801965956
Grebenchtchikova <greben@stanford.edu>
2026-05-30 18:37:10 +0100

    Update modal_convert_ckpt.py

--- modal_convert_ckpt.py
+++ modal_convert_ckpt.py
@@ -20,7 +20,8 @@ app = modal.App("cs224r-trivia-convert")
 
 image = (
     modal.Image.from_dockerfile("docker/Dockerfile.ngc.vllm0.8.noverl")
-    .run_commands("pip install --no-deps verl==0.2.0.post2", "pip install fire")
+    .add_local_dir("../cs224r-project-e3", "/root/verl", copy=True)
+    .run_commands("pip install -e /root/verl", "pip install fire")
     .add_local_file("convert_fsdp_to_hf.py", "/root/convert_fsdp_to_hf.py")
 )
 
